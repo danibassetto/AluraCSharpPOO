008869ec46135e7ca5df7827f98968b9282c2458
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-01-07 18:05:35 -0300

    new

---
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,14 @@
+﻿namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+        
+        public double GetBonificacao()
+        {
+            return this.Salario * 0.1; // bonificação corresponde a 10% do salário
+        }
+    }
+}
---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,33 @@
+﻿using bytebank_ADM.Funcionarios;
+using bytebank_ADM.Utilitario;
+
+#region Aula 1 - Sobrecarga e Polimorfismo
+/* 
+Nessa aula temos um exemplo de sobrecarga e polimorfismo com o método Registrar, que pode receber parâmetros diferentes
+
+Podemos ter comportamentos definidos em uma classe que funcionem diferentemente dependendo dos parâmetros que receberem. Definimos uma 
+sobrecarga que registra o total de bonificação para objetos do tipo funcionário e para diretor, o que se encaixa num conceito deste modelo
+de programação.
+*/
+Funcionario pedro = new Funcionario();
+pedro.Nome = "Pedro Silva";
+pedro.Cpf = "1234567891";
+pedro.Salario = 2000;
+
+Console.WriteLine(pedro.Nome);
+Console.WriteLine(pedro.GetBonificacao());
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta Silva";
+roberta.Cpf = "9876543212";
+roberta.Salario = 5000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.GetBonificacao());
+
+GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+gerenciador.Registrar(pedro);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine("Total de bonificações: " + gerenciador.TotalDeBonificacao);
+#endregion
