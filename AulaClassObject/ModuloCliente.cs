f7977e1d7d3c1ffc1caec63b3f39101a81a0485c
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-01-18 23:03:28 -0300

    New

---
+++ AulaClassObject/ModuloCliente.cs
@@ -0,0 +1,18 @@
+﻿namespace ModuloCliente
+{
+    public class Cliente
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string? Email { get; set; }
+        public int Idade { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {this.Nome}, \n" +
+                   $"Email: {this.Email}, \n" +
+                   $"Idade: {this.Idade}, \n" +
+                   $"Cpf: {this.Cpf}";
+        }
+    }
+}
---
+++ AulaClassObject/Program.cs
@@ -0,0 +1,23 @@
+﻿using ModuloCliente;
+
+Cliente cliente = new Cliente();
+cliente.Nome = "André";
+cliente.Cpf = "111.444.666-88";
+cliente.Email = "andre@email.com";
+cliente.Idade = 23;
+
+Console.WriteLine(cliente.Nome);
+Console.WriteLine(cliente.Cpf);
+Console.WriteLine(cliente.Idade);
+
+Console.WriteLine(cliente.ToString());
+
+/* 
+Observando os metadados, notamos que a classe Object também tem alguns métodos disponíveis. Por exemplo, na linha 22, temos public virtual bool 
+Equals(object? obj) — considerando nossos conhecimentos das regras de sobrecarga e sobrescrita, sabemos que esse método deve ser sobrescrito. 
+Temos também GetHashCode(), GetType(), MemberwiseClone(), ReferenceEquals() e ToString().
+
+Se nossa classe é um object, quer dizer que temos todos esses métodos disponíveis nela!
+Todas as classes do . NET herdam explícita ou implicitamente da classe Object.
+
+*/
